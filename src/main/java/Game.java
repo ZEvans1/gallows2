03679e3abd94b0b8a5e24655815b1278d45032e4
ze <zte.zachary@gmail.com>
2018-01-03 16:46:05 -0800

    Add more

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Game {
+    private String[] letterList;
+    private String letter;
+
     private ArrayList<String> wordList = new ArrayList<>();
     public ArrayList<String> makeList() {
         wordList.add("jazz");
@@ -10,10 +13,19 @@ public class Game {
         return wordList;
     }
 
-    public Game (String letterguess) {
+    public Game (String letterGuess) {
         makeList();
+        letter = letterGuess;
         Random random = new Random();
         String selectWord = wordList.get(0);
+        letterList = selectWord.split("");
 //        String selectWord = wordList.get( random.nextInt(wordList.size()) ); //this will show "jazz"
+        for (int i = 0; i < selectWord.length(); i++) {
+            if (letter.equals(letterList[i])) {
+                //command
+            } else {
+                //other command
+            }
+        }
     }
 }
